0e03950c6b09bb166a4c5c7b81a2b5a9e34615e1
tsuchy298 <akio@goukaku-suppli.com>
2017-02-20 08:30:58 +0000

    using Bootstrap_20170220_1730

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+  #　set_messageをeditとupdateの前に実行する
+  #before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     @message = Message.new
     # Messageを全て取得する。
@@ -10,10 +13,32 @@ class MessagesController < ApplicationController
     @message.save
     redirect_to root_path , notice: 'メッセージを保存しました'
   end
-
+  
+  # def edit
+  # end
+  
+  # def update
+  #   if @message.update(message_params)
+  #     # 保存に成功した場合はトップページへリダイレクト
+  #     redirect_to root_path, notice: 'メッセージを編集しました。'
+  #   else
+  #     #保存に失敗した場合は編集画面へ戻す
+  #     render "edit"
+  #   end
+  # end
+  
+  # def destroy
+  #   @message.destroy
+  #   redirect_to root_path, notice: "メッセージを削除しました。"
+  # end
+  
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
+  
+  # def set_message
+  #   @message = Message.find(params[:id])
+  # end
+  
 end
